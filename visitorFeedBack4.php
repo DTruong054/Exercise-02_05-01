97a4d8a149572a637a4daff12bcbf3b6061cdb07
Unknown <DTruong054@west-mec.org>
2018-10-12 15:26:25 -0700

    \OwO/
    \Owo

---
+++ visitorFeedBack4.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Visitor Feedback 3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+
+<body>
+    <h2>Visitor Feedback 3</h2>
+    <?php
+        $dir = "./comments";
+        if (is_dir($dir)) {
+            $commentFiles = scandir($dir);
+            foreach ($commentFiles as $fileName) {
+                if ($fileName !== "." && $fileName !== "..") {
+                    echo "From <strong>$fileName</strong><br>";
+                    $filehandle = fopen($dir . "/" . $fileName, "rb");
+                    if (!$filehandle) {
+                        echo "There was an error reading file\"" . "\$fileName\".<br>\n";
+                    } else {
+                        $from = fgets($filehandle);
+                        echo "From: " . htmlentities($from) . "<br>\n";
+                        echo "</hr>\n";
+                        fclose($filehandle);
+                    }
+                    // echo "Email: " . htmlentities($comments[1]) . "<br>\n";
+                    // echo "Date: " . htmlentities($comments[2]) . "<br>\n";
+                    // $commentLines = count($comments);
+                    // for ($i = 3; $i < $commentLines; $i++) { 
+                    //     echo htmlentities($comments[$i]) . "<br>\n";
+                    // }
+                    echo "<hr>\n";
+                }
+            }
+        } else {
+            echo "Folder \"$dir\" does not exist";
+        }
+        
+    ?>
+</body>
+
+</html>
